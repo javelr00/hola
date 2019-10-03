f52288604bd578f6cc0e53008518afd9dfed5898
javelr00 <javelr00@estudiantes.unileon.es>
2019-10-03 09:37:35 +0200

    Primer commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,14 @@
+
+/**
+    * Clase HolaMundo
+    * @ author Javi
+    */
+public class HolaMundo {
+    	/** Metodo que imprime 
+	*   Hola Mundo
+	*/
+    	public static void main(String[] args) {		
+    		System.out.println("Hola Mundos");
+    	}
+     
+    }
